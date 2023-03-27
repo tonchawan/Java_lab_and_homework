750ce1cb4e6a4ddc19553cc53a5650802f1da584
tonchawan <tonchawan50@gmail.com>
2023-03-27 14:56:04 +0700

    test day2

---
+++ Day2/Computer.java
@@ -0,0 +1,14 @@
+package Day2;
+public class Computer {
+    public void createDocument() {
+        System.out.println("Creating a new document...");
+    }
+
+    public void printDocument() {
+        System.out.println("Printing the current document...");
+    }
+
+    public void internetSurfing() {
+        System.out.println("Surfing the internet...");
+    }
+}
---
+++ Day2/Desktop.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class Desktop extends Computer {
+    public void startMenu() {
+        System.out.println("Opening the Start menu...");
+    }
+
+    public void microsoftStore() {
+        System.out.println("Opening the Microsoft Store...");
+    }
+}
